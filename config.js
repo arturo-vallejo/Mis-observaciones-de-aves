58138825ff44368fb35ed5c2a9b7713be0a4510b
arturo-vallejo <a.vallejo@tec.mx>
2026-02-06 14:52:51 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
   //    → Abre la tabla en Baserow, mira la URL:
   //      https://baserow.io/database/XXX/table/YYYY
   //      El ID es YYYY
-  BASEROW_TABLE_ID: "yauGcOlmywXEow9m16Q2zYT4YoOYuA84",
+  BASEROW_TABLE_ID: "1620405",
 
   // 3. URL de la API de Baserow
   //    → Si usas Baserow en la nube, déjalo así.
